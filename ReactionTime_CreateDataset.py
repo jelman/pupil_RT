d55aa2df4cf62bb38ed5919a1fa7c82bf155df4c
Jeremy Elman <jelman523@gmail.com>
2015-07-17 08:43:21 -0700

    add functions to calculcate summary statistics of subject performance

--- ReactionTime_CreateDataset.py
+++ ReactionTime_CreateDataset.py
@@ -31,17 +31,126 @@ def set_miss_RT(df):
     """ Set any trial with inaccurate response to a missing RT. """    
     df.loc[df['Stimulus.ACC']==0,'Stimulus.RT'] = np.nan 
     return df
+
+def calc_hits(trialdf):
+    """ Calculate hits (correct responses) """
+    return (trialdf['Stimulus.ACC']==1).sum()  
+    
+def calc_misses(trialdf):
+    """ Calculate misses (incorrect response) """
+    misses = ((trialdf['Stimulus.ACC']==0) & 
+                (trialdf['Stimulus.RESP'].notnull())).sum()
+    return misses
+    
+def calc_NR(trialdf):
+    """ Calculate no responses """
+    NR = ((trialdf['Stimulus.ACC']==0) & 
+                (trialdf['Stimulus.RESP'].isnull())).sum()
+    return NR
+
+def calc_medianRT(trialdf):
+    """ Calculate median RT for correct trials. """
+    return trialdf.ix[trialdf['Stimulus.ACC']==1,'Stimulus.RT'].median()    
+
+def calc_meanRT(trialdf):
+    """ Calculate mean RT for correct trials. """
+    return trialdf.ix[trialdf['Stimulus.ACC']==1,'Stimulus.RT'].mean()    
+
+def calc_stdRT(trialdf):
+    """ Calculate standard deviation of RT for correct trials. """
+    return trialdf.ix[trialdf['Stimulus.ACC']==1,'Stimulus.RT'].std()    
+
+def calc_trimmed_meanRT(trialdf, meanRT, stdRT):
+    """ Calculate trimmed mean of RT for correct trials. Excludes any 
+    trials that fall outside of 3 standard deviations of the mean. """
+    idx = ((trialdf['Stimulus.ACC']==1) &
+            (trialdf['Stimulus.RT'] > meanRT-(3*stdRT)) &
+            (trialdf['Stimulus.RT'] < meanRT+3*(stdRT)))
+    return trialdf.ix[idx,'Stimulus.RT'].mean()
+
+def calc_cvRT(meanRT, stdRT):
+    """ Calculate coefficient of variation of RT for correct trials. 
+    Divides the standard deviation of RT by mean RT. """
+    return meanRT / stdRT
+
+def calc_trial_scores(trialdf):
+    """ 
+    Calculates summary scores for a given trial type. 
+    Input is a dataframe containing trials of one trial type from
+    one subject.
+    Output is a series where each observation is named by the 
+    summary score.
+    """
+    hits =  calc_hits(trialdf)
+    misses =  calc_misses(trialdf)
+    NR = calc_NR(trialdf)
+    meanRT = calc_meanRT(trialdf)
+    stdRT = calc_stdRT(trialdf)
+    trimmed_meanRT = calc_trimmed_meanRT(trialdf, meanRT, stdRT)
+    cvRT = calc_cvRT(meanRT, stdRT)
+    summary_scores = pd.Series({'hits': hits, 'misses': misses, 'NR': NR,
+                        'meanRT': meanRT, 'trimmed_meanRT': trimmed_meanRT,
+                        'stdRT': stdRT, 'cvRT': cvRT})
+    return summary_scores
+
+def calc_subject_scores(subjectdf):
+    """
+    Calculates summary scores for each subject, iterating over trial types. 
+    Input is a dataframe containing all trial types for one subject. 
+    Output contains one row per trial type and one column per summary score.
+    """
+    return subjectdf.groupby('TrialType').apply(calc_trial_scores)  
+
+def summarise_subjects(df):
+    """
+    Calculates summary scores for the group, iterating over subjects. 
+    Input is a dataframe containing all trial types for all subjects.
+    Output is transformed such that each row is a subject, and each
+    column is a combination of trial type and summary score.
+    """
+    summarydf = df.groupby('SubjectID').apply(calc_subject_scores)
+    summarydf = summarydf.unstack()
+    summarydf = summarydf.reorder_levels([1,0], axis=1)
+    summarydf.columns = [''.join(col).strip().lower() 
+                            for col in summarydf.columns.values]  
+    return summarydf
+
+def calc_hitmiss_rt(hits, misses):
+    """ Given the number of hits and misses for a particular trial type, 
+    calculates the hit rate and miss rate. """
+    hitrt = hits / (hits + misses)
+    missrt = 1. - hitrt    
+    return hitrt, missrt
+    
+def get_hitmiss_rt(summed_df, trialtypes=['Simple','Choice']):
+    """ Loops over trial types and inserts hit and miss rate for each into 
+    the passed dataframe. """
+    for trial in trialtypes:
+        trial = trial.lower()
+        hits = summed_df[''.join([trial,'hits'])]
+        misses = summed_df[''.join([trial,'misses'])]
+        hitrtvarname = ''.join([trial,'hitrt'])
+        missrtvarname = ''.join([trial,'missrt'])
+        summed_df[hitrtvarname], summed_df[missrtvarname] = calc_hitmiss_rt(hits,misses)
+    return summed_df
+
+def apply_excludes(rt_rates):
+    """ Placeholder function. """
+    return rt_rates
     
 def main(infile, outfile):
     rt_raw = pd.read_csv(infile, sep=',')
     rt_filt = apply_filters(rt_raw)
     rt_filt = set_miss_RT(rt_filt)
-    
+    rt_summed = summarise_subjects(rt_filt)
+    rt_rates = get_hitmiss_rt(rt_summed)
+    rt_clean = apply_excludes(rt_rates)
+    rt_clean.to_csv(outfile, index=False)    
     
 ##############################################################
 ############## Set paths and parameters ######################
 ##############################################################
-datapath = 'K:/ReactionTime/data' # Specify data path of RT data
+datapath = 'K:/data/ReactionTime/UCSD' # Specify data path of RT data
 fname = 'ReactionTime_UCSD_merged.csv' # Name of input data file
 infile = os.path.join(datapath,fname) # Input file
 outname = 'ReactionTime_UCSD_processed.csv' # Name of file to save out

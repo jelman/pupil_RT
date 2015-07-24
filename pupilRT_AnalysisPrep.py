816dd3f13f328a5ea5b685ec7d89a7fe5a3e1e47
Jeremy Elman <jelman523@gmail.com>
2015-07-24 14:11:56 -0700

    add lines to merge RT data with cog and pupil data

--- pupilRT_AnalysisPrep.py
+++ pupilRT_AnalysisPrep.py
@@ -10,9 +10,10 @@ import numpy as np
 from sas7bdat import SAS7BDAT
 import os
 
+# Data directory
 datadir = 'K:/ReactionTime/data'
 
-### Get cognitive data ###
+## Get cognitive data ###
 # Load cognitive scores
 fname = 'K:/pupillometry/data/cognitive/vetsa2merged_23apr2015.sas7bdat'
 with SAS7BDAT(fname) as f:
@@ -35,8 +36,39 @@ cogdf.ix[cogdf['NUMHINJ_v2']==99,'NUMHINJ_v2'] = None
 outfile = os.path.join(datadir, 'cogData.csv')
 cogdf.to_csv(outfile, index=False)
 
-## Load pupil data
+# Load pupil data
 pupildf = pd.read_csv('K:/data/Pupillometry/pupilDS_long.csv')
 
-## Load Reaction Time data
-axcptdf = pd.read_csv('K:/data/ReactionTime/ReactionTime_processed.csv')
+# Load Reaction Time data
+RTdf = pd.read_csv('K:/data/ReactionTime/ReactionTime_processed.csv')
+simpleRT = RTdf[RTdf['TrialType']=='Simple']
+choiceRT = RTdf[RTdf['TrialType']=='Choice']
+
+## Merge datasets
+# Simple RT
+pupil_simpleRT = pd.merge(pupildf, simpleRT, left_on='vetsaid', 
+                   right_on='SubjectID', how='left')                  
+pupil_simpleRT = pd.merge(pupil_simpleRT, cogdf, left_on='vetsaid', 
+                   right_on='vetsaid', how='left')
+pupil_simpleRT = pupil_simpleRT.drop(['case_y','twin_y','zyg14_y',
+                                      'SubjectID'], axis=1)
+pupil_simpleRT = pupil_simpleRT.rename(columns={'case_x':'case',
+                                                'twin_x':'twin',
+                                                'zyg14_x':'zyg14'})
+# Choice RT                                                
+pupil_choiceRT = pd.merge(pupildf, choiceRT, left_on='vetsaid', 
+                   right_on='SubjectID', how='left')
+pupil_choiceRT = pd.merge(pupil_choiceRT, cogdf, left_on='vetsaid', 
+                   right_on='vetsaid', how='left')    
+pupil_choiceRT = pupil_choiceRT.drop(['case_y','twin_y','zyg14_y',
+                                      'SubjectID'], axis=1)                   
+pupil_choiceRT = pupil_choiceRT.rename(columns={'case_x':'case',
+                                                'twin_x':'twin',
+                                                'zyg14_x':'zyg14'})                   
+
+# Save out files
+simple_outfile = os.path.join(datadir,'pupil_simpleRT.csv')
+pupil_simpleRT.to_csv(simple_outfile, index=False)
+
+choice_outfile = os.path.join(datadir,'pupil_choiceRT.csv')
+pupil_choiceRT.to_csv(choice_outfile, index=False)

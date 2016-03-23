d8fc6e169303c8a761478a6906c5189c444a7760
Jeremy Elman <jelman523@gmail.com>
2016-03-23 15:57:12 -0700

    add raw, z-scored, and composite variables from cognitive domains dataset

--- pupilProcSpeed_AnalysisPrep.py
+++ pupilProcSpeed_AnalysisPrep.py
@@ -5,10 +5,9 @@ Created on Thu Mar 10 12:51:55 2016
 @author: jaelman
 """
 
-
+from sas7bdat import SAS7BDAT
 import pandas as pd
 import numpy as np
-from sas7bdat import SAS7BDAT
 import os
 
 ###############################
@@ -16,36 +15,41 @@ datadir = 'K:/Projects/Pupil_ReactionTime/data'
 pupil_fname = 'K:/data/Pupillometry/pupilDS_long.csv'
 cogv2_fname = 'K:/data/VETSA2_April2015/vetsa2merged_1dec2015_edits.sas7bdat'
 demo_fname = 'K:/data/VETSA_demo_vars.csv'
-cogVars_fname = 'K:/Projects/Pupil_ReactionTime/data/ProcSpeed_CogVariables.csv'
-cogdomain_fname = 'K:/Projects/Cognitive Domains/data/V2_CognitiveDomains.csv'
+cogdomainV1_fname = 'K:/Projects/Cognitive Domains/data/V1_CognitiveDomains_All.csv'
+cogdomainV2_fname = 'K:/Projects/Cognitive Domains/data/V2_CognitiveDomains_All.csv'
 cog_outname = 'ProcSpeed_cogData.csv'
 mci_fname = 'K:/data/VETSA2_MCI.csv'
 pupilPS_outname = 'pupil_ProcSpeed.csv'
+procspeedVars = ['zProcSpeed','zstrwraw','zstrcraw','ztrl2tran','ztrl3tran',
+                 'strwraw','strcraw','TRL2TRAN','TRL3TRAN']
 ###############################
 
 ### Get cognitive and demographic data ###
 
-# Load domegraphic data
+# Load demographic data
 demodf = pd.read_csv(demo_fname)
 
-# Load cognitive scores
+# Load vetsa2merged dataset to get head injury data
 with SAS7BDAT(cogv2_fname) as f:
     cogdf = f.to_data_frame()
     
-cogvars = pd.read_csv(cogVars_fname)
-cogdf = cogdf[cogvars['NAME']]
+cogdf = cogdf[['vetsaid','HADSHINJ_v2','NUMHINJ_v2']]
 
 # Set missing missing values in head injury variables
 cogdf.ix[cogdf['HADSHINJ_v2']==9,'HADSHINJ_v2'] = None
 cogdf.ix[cogdf['NUMHINJ_v2']==99,'NUMHINJ_v2'] = None
 
 # Merge demographic and cognitive data
-cogdf = demodf.merge(cogdf, on='vetsaid')
+cogdf = demodf.merge(cogdf, how='right', on='vetsaid')
 
 # Load and merge Proc Speed cognitive domain data
-cogdomaindf = pd.read_csv(cogdomain_fname)
-cogdomaindf = cogdomaindf[['vetsaid','zProcSpeed_v2']]
-cogdf = cogdf.merge(cogdomaindf, on='vetsaid')
+cogdomainV1df = pd.read_csv(cogdomainV1_fname)
+cogdomainV1df = cogdomainV1df[['vetsaid']+procspeedVars]
+cogdomainV2df = pd.read_csv(cogdomainV1_fname)
+cogdomainV2df = cogdomainV2df[['vetsaid']+procspeedVars]
+cogdf = cogdf.merge(cogdomainV1df, how='left', on='vetsaid')
+cogdf = cogdf.merge(cogdomainV2df, how='left', on='vetsaid', 
+                    suffixes=('_v1','_v2'))
 
 # Save out cognitive data
 cog_outfile = os.path.join(datadir, cog_outname)

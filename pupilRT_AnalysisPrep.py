08ff5b1b73e270ae68fc2709dc088727e331ba60
jelman <jelman523@gmail.com>
2017-01-05 10:00:08 -0800

    Use practice effect adjusted data and combine choice and simple RT datasets

--- pupilRT_AnalysisPrep.py
+++ pupilRT_AnalysisPrep.py
@@ -10,31 +10,30 @@ from sas7bdat import SAS7BDAT
 import os
 
 ###############################
-datadir = 'K:/Projects/Pupil_ReactionTime/data'
-pupil_fname = 'K:/data/Pupillometry/pupilDS_long.csv'
-cogv2_fname = 'K:/data/VETSA2_April2015/vetsa2merged_1dec2015_edits.sas7bdat'
-cogv1_fname = 'K:/data/VETSA1_Aug2014/vetsa1merged_21aug2014.sas7bdat'
-cogVars_fname = 'K:/Projects/Pupil_ReactionTime/data/ReactionTime_CogVariables.csv'
-demo_fname = 'K:/data/VETSA_demo_vars.csv'
-rt_fname = 'K:/data/ReactionTime/ReactionTime_processed.csv'
+datadir = '/home/jelman/netshare/K/Projects/Pupil_ReactionTime/data'
+pupil_fname = '~/netshare/K/data/Pupillometry/VETSA2/pupilDS_long.csv'
+cog_fname = '/home/jelman/netshare/M/PSYCH/KREMEN/Practice Effect Cognition/data/V1V2_CogData_NASAdj_PE.csv'
+demo_fname = '~/netshare/K/data/VETSA_Demographics/VETSA_demo_vars2.csv'
+rt_fname = '/home/jelman/netshare/K/data/ReactionTime/ReactionTime_processed.csv'
 cog_outname = 'ReactionTime_cogData.csv'
-mci_fname = 'K:/data/VETSA2_MCI.csv'
-simple_outname = 'pupil_simpleRT.csv'
-choice_outname = 'pupil_choiceRT.csv'
+mci_fname = '~/netshare/K/data/VETSA_Demographics/VETSA2_MCI.csv'
+outname = 'pupil_RT.csv'
 ###############################
 
 # Load demographic data
 demodf = pd.read_csv(demo_fname)
 
-# Load vetsa2merged dataset to get head injury data
-with SAS7BDAT(cogv2_fname) as f:
-    cogdf = f.to_data_frame()
-    
-cogdf = cogdf[['vetsaid','DSFMAX_v2','HADSHINJ_v2','NUMHINJ_v2']]
+# Load practice effects data to get RT data
+cogdf = pd.read_csv(cog_fname)
 
-# Set missing missing values in head injury variables
-cogdf.ix[cogdf['HADSHINJ_v2']==9,'HADSHINJ_v2'] = None
-cogdf.ix[cogdf['NUMHINJ_v2']==99,'NUMHINJ_v2'] = None
+cols = ['VETSAID','NAS201TRAN','DSFMAX_V2_nasp','SRTLMEANLOG_V2_nasp',
+        'SRTLSTDLOG_V2_nasp','SRTRMEANLOG_V2_nasp','SRTRSTDLOG_V2_nasp',
+        'SRTGMEANLOG_V2_nasp','SRTGSTDLOG_V2_nasp','CHRTLMEANLOG_V2_nasp',
+        'CHRTRMEANLOG_V2_nasp','CHRTLSTDLOG_V2_nasp','CHRTRSTDLOG_V2_nasp',
+        'CHRTGMEANLOG_V2_nasp','CHRTGSTDLOG_V2_nasp']
+cogdf.columns
+cogdf = cogdf.loc[:,cols]
+cogdf.rename(columns={'VETSAID':'vetsaid'}, inplace=True)
 
 # Merge demographic and cognitive data
 cogdf = demodf.merge(cogdf, how='right', on='vetsaid')
@@ -47,38 +46,26 @@ cogdf.to_csv(cog_outfile, index=False)
 pupildf = pd.read_csv(pupil_fname)
 pupildf = pupildf.drop(['case', 'twin', 'zyg14'], axis=1)
 
-# Load Reaction Time data
+# Load Reaction Time qc data
 RTdf = pd.read_csv(rt_fname)
-simpleRT = RTdf[RTdf['TrialType']=='Simple']
-choiceRT = RTdf[RTdf['TrialType']=='Choice']
+RTdf = RTdf.loc[RTdf['TrialType']=='Simple', ['vetsaid','ZRT_v2','RTCOMPLETE_v2']]
 
 # Load MCI data
 MCIdf = pd.read_csv(mci_fname)
 
 #Filter out subjects who were given a Z score of 2 or were not completed
-simpleRT = simpleRT.loc[(simpleRT['ZRT_v2']!=2) & 
-                      (simpleRT['RTCOMPLETE_v2']==0)]
-                      
-choiceRT = choiceRT.loc[(choiceRT['ZRT_v2']!=2) & 
-                      (choiceRT['RTCOMPLETE_v2']==0)]    
-                      
+RTdf = RTdf.loc[(RTdf['ZRT_v2']!=2) &
+                      (RTdf['RTCOMPLETE_v2']==0)]
+
+
 ## Merge datasets
 
-# Simple RT                  
-pupil_simpleRT = pd.merge(cogdf, pupildf, on='vetsaid', how='right')                      
-pupil_simpleRT = pd.merge(pupil_simpleRT, simpleRT, on='vetsaid', how='inner')                  
-pupil_simpleRT = pd.merge(pupil_simpleRT, MCIdf[['vetsaid','rMCI_cons_v2']], 
-                         on='vetsaid', how='left')   
-                             
-# Choice RT                                                
-pupil_choiceRT = pd.merge(cogdf, pupildf, on='vetsaid', how='right')                      
-pupil_choiceRT = pd.merge(pupil_choiceRT, choiceRT, on='vetsaid', how='inner')                  
-pupil_choiceRT = pd.merge(pupil_choiceRT, MCIdf[['vetsaid','rMCI_cons_v2']], 
-                         on='vetsaid', how='left')   
-                         
-# Save out files
-simple_outfile = os.path.join(datadir,simple_outname)
-pupil_simpleRT.to_csv(simple_outfile, index=False)
+# Simple RT
+pupil_RT = pd.merge(cogdf, pupildf, on='vetsaid', how='right')
+pupil_RT = pd.merge(pupil_RT, RTdf, on='vetsaid', how='inner')
+pupil_RT = pd.merge(pupil_RT, MCIdf[['vetsaid','rMCI_cons_v2']],
+                         on='vetsaid', how='left')
 
-choice_outfile = os.path.join(datadir,choice_outname)
-pupil_choiceRT.to_csv(choice_outfile, index=False)
+# Save out files
+outfile = os.path.join(datadir,outname)
+pupil_RT.to_csv(outfile, index=False)
